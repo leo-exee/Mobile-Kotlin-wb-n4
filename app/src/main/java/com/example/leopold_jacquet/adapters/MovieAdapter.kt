c91bb09e437ccef93ad1a364a1353d5f77b444a4
leo-exee <leopold@littlebill.io>
2024-04-16 12:03:05 +0200

    fix(import): remove unused import

--- app/src/main/java/com/example/leopold_jacquet/adapters/MovieAdapter.kt
+++ app/src/main/java/com/example/leopold_jacquet/adapters/MovieAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.leopold_jacquet.adapters
 
 import android.annotation.SuppressLint
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
